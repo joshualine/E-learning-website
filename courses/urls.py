0df179c4c8ef92fd0d56ebea65c14d7ada2a6390
Joshaline <joshuachinwendu@gmail.com>
2021-03-20 00:37:02 +0100

    send commit

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
+from . import views
 from .views import HomePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('mine/',
+         views.ManageCourseListView.as_view(),
+         name='manage_course_list'),
+
+    path('create/',
+         views.CourseCreateView.as_view(),
+         name='course_create'),
+
+    path('<pk>/edit/',
+         views.CourseUpdateView.as_view(),
+         name='course_edit'),
+    path('<pk>/delete/',
+         views.CourseDeleteView.as_view(),
+         name='course_delete'),
 ]
